b9700ecc334b55c14577042e1e21efe1d9a45c05
Naman Seth <naman.seth_cs19@gla.ac.in>
2020-08-07 12:19:31 +0530

    Code...

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Seth (namanseth01)
+ *   Date: 07-Aug-20
+ *   Time: 12:04 PM
+ *   File: HelloWorld.java
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
