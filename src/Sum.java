0c377a388fd22c7e1eb26d3c6eebe4dc0d4360b1
Naman Seth <naman.seth_cs19@gla.ac.in>
2020-08-07 12:27:52 +0530

    Addition of two numbers...

---
+++ src/Sum.java
@@ -0,0 +1,14 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Naman Seth (namanseth01)
+ *   Date: 07-Aug-20
+ *   Time: 12:22 PM
+ *   File: Sum.java
+ */
+
+public class Sum {
+    public static void main(String[] args) {
+        int number1 = 52;
+        int number2 = 53;
+        System.out.println(number1 + number2);
+    }
+}
